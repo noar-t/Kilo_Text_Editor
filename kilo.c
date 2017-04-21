52704bac176afebe0ba1cc5707976b87f11bab80
noar-t <noahthornton15@gmail.com>
2017-04-20 21:08:27 -0500

    Initial Commit

---
+++ kilo.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;                     //default values so we can set upon exit
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);                        //set disableRawMode to be called upon exit
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);                        //sets raw input flag
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  return 0;
+}
