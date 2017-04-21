7f61c19546e539810951fefc6d63f7206d4301c6
noar-t <noahthornton15@gmail.com>
2017-04-21 03:13:24 -0500

    continued output

--- kilo.c
+++ kilo.c
@@ -4,11 +4,15 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** defines ***/
+
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -71,11 +75,39 @@ char editorReadKey() {  //waits and reads in a valid char and returns it
   return c;
 }
 
-int getWindowSize(int *rows, int *cols) {
+
+int getCursorPosition(int *rows, int *cols) { //get positon of cursor in terminal:
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) 
+    return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i] , 1) != 1)
+      break;
+    if (buf[i] == 'R') 
+      break;
+    i++;  
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') 
+    return -1;
+  if (sscanf(&buf[2], "%d:%d", rows,cols) != 2)
+    return -1;
+
+  return 0;
+}
+
+
+int getWindowSize(int *rows, int *cols) { //gets size of terminal
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)  //puts curor bottom left
+      return -1;
+    return getCursorPosition(rows, cols);
   }
   else {
     *cols = ws.ws_col;
@@ -84,23 +116,65 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/***append buffer ***/
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) { //putting chars into a buffer
+  char *new = realloc(ab->b, ab->len + len);
+  if (new == NULL)
+    return ;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) { //frees space/destructure
+  free(ab->b);
+}
+
 /*** output ***/
 
-void editorDrawRows() { // draws tilde on left like vim
+void editorDrawRows(struct abuf *ab) { // draws tilde on left like vim
   int y;
-  for (y = 0; y < 24; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+  for (y = 0; y < E.screenrows; y++) {
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols)
+        welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+    }
+    else {
+      abAppend(ab, "~", 1);
+    }
+
+    abAppend(ab, "\x1b[K", 3); //clear line
+    if (y < E.screenrows - 1)
+      abAppend(ab, "\r\n", 2);
   }
 }
 
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4); //escape sequence (x1b), then clear whole screen
-  write(STDOUT_FILENO, "\x1b[H", 3);  //cursor top lefta
+  struct abuf ab = ABUF_INIT;
+  
+  abAppend(&ab, "\x1b[?25l", 6); //hide cursor sp dpesnt flicker
+  abAppend(&ab, "\x1b[H", 3);  //cursor top lefta
 
-  editorDrawRows();
+  editorDrawRows(&ab);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);  //cursor top lefta
+  abAppend(&ab, "\x1b[H", 3);  //cursor top lefta
+  abAppend(&ab, "\x1b[?25h", 6); //show cursor
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 /*** input ***/
@@ -120,10 +194,18 @@ void editorProcessKeypress() { //process char from editorReadKey()
 
 /*** init ***/
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) 
+    die("getWindowSize");
+}
+
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
