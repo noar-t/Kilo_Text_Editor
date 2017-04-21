4cf23516239ab1543249e50ab9ec937e668350a6
noar-t <noahthornton15@gmail.com>
2017-04-20 23:23:37 -0500

    Some more flags for raw input mode

--- kilo.c
+++ kilo.c
@@ -14,8 +14,11 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);                        //set disableRawMode to be called upon exit
 
+  // various flags to completely enable raw mode
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(ICRNL | IXON);                //carrigae return | set stop/resume transmission
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);        //sets raw input flag | canoncial flag | disable literal input | siginit/sigtstp
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -25,12 +28,12 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') { //read into char c continously except 'q'
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     }
     else{
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
   return 0;
