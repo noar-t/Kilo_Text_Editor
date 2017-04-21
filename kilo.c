a55b97551d084b918fede0fe6fc7b17468e39e70
noar-t <noahthornton15@gmail.com>
2017-04-21 13:10:07 -0500

    Added handling for pageup/down

--- kilo.c
+++ kilo.c
@@ -16,10 +16,12 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
-  ARROW_LEFT = 'a',
-  ARROW_RIGHT = 'd',
-  ARROW_UP = 'w',
-  ARROW_DOWN = 's'
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -72,7 +74,7 @@ void enableRawMode() {  //puts terminal into raw mode
 }
 
 
-char editorReadKey() {  //waits and reads in a valid char and returns it
+int editorReadKey() {  //waits and reads in a valid char and returns it
   int nread;
   char c;
 
@@ -90,11 +92,23 @@ char editorReadKey() {  //waits and reads in a valid char and returns it
       return '\x1b';
 
     if (seq[0] == '[') {
-      switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') { //handles pageup, pagedown
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      }
+      else { 
+        switch (seq[1]) { //handes arrow key input
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+        }
       }
     }
 
@@ -221,26 +235,30 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
-void editorMoveCursor(char key) {//increments/decrements cursor position
+void editorMoveCursor(int key) {//increments/decrements cursor position
   switch (key) {
     case ARROW_LEFT:
-      E.cx--;
+      if (E.cx != 0)
+        E.cx--;
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if (E.cx != E.screencols - 1)
+        E.cx++;
       break;
     case ARROW_UP:
-      E.cy--;
+      if (E.cy != 0)
+        E.cy--;
       break;
     case ARROW_DOWN:
-      E.cy++;
+      if (E.cy != E.screenrows - 1)
+        E.cy++;
       break;
   }
 }
 
 
 void editorProcessKeypress() { //process char from editorReadKey()
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch (c) {
     case CTRL_KEY('q'):
