568c6bda0d19fdc06b3d38aba480aa2f7a36ae28
noar-t <noahthornton15@gmail.com>
2017-04-20 23:36:26 -0500

    Error method and polling added

--- kilo.c
+++ kilo.c
@@ -1,16 +1,22 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-struct termios orig_termios;                     //default values so we can set upon exit
+struct termios orig_termios;                     //default values of terminal so we can reset upon exit
 
-void disableRawMode() {
+void die(const char *s) {  //exits with nonzero value setting errno to indicate what the error was
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {  //puts terminal into raw mode
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-void enableRawMode() {
+void enableRawMode() {  //puts terminal into raw mode
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);                        //set disableRawMode to be called upon exit
 
@@ -21,6 +27,11 @@ void enableRawMode() {
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);        //sets raw input flag | canoncial flag | disable literal input | siginit/sigtstp
 
+  //indexes for c_cc field which controls behavior of read, sets timeout to 1/10 of a sec
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -28,13 +39,19 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') { //read into char c continously except 'q'
+  while (1) {
+
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     }
     else{
       printf("%d ('%c')\r\n", c, c);
     }
+    
+    if (c == 'q') break;
   }
   return 0;
 }
