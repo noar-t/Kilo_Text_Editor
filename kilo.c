7ba19aac01ebcac62df7d2f87345809442bb36bc
noar-t <noahthornton15@gmail.com>
2017-07-23 17:08:13 -0500

    highlighting clears after a search now

--- kilo.c
+++ kilo.c
@@ -494,6 +494,15 @@ void editorSave() {
 void editorFindCallback(char *query, int key) { // function to continously search
   static int last_match = -1;
   static int direction = 1;
+
+  static int saved_hl_line;
+  static char *saved_hl = NULL;
+
+  if (saved_hl) {
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
   
   
   if (key == '\r' || key == '\x1b') { // logic for moving forward and back
@@ -531,6 +540,9 @@ void editorFindCallback(char *query, int key) { // function to continously searc
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
 
+      saved_hl_line = current;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
       memset(&row->hl[match - row->render], HL_MATCH, strlen(query)); // hihglight matched search
       break;
     }
