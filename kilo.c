e6d62848262dbdd96eaef693743c430f6e0b056d
noar-t <noahthornton15@gmail.com>
2017-07-23 17:12:48 -0500

    refactoring number highlighting

--- kilo.c
+++ kilo.c
@@ -219,11 +219,16 @@ int getWindowSize(int *rows, int *cols) { // gets size of terminal
 void editorUpdateSyntax(erow *row) {
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
-  int i;
-  for (i = 0; i < row->size; i++) {
-    if (isdigit(row->render[i])) {
+
+  int i = 0;
+  while (i < row->size) {
+    char c = row->render[i];
+
+    if (isdigit(c)) {
       row->hl[i] = HL_NUMBER;
     }
+
+    i++;
   }
 }
 
