34c2a107ea638a7f9eaee936ea9d42678d001128
noar-t <noahthornton15@gmail.com>
2017-07-23 17:28:09 -0500

    fix in number highlighting

--- kilo.c
+++ kilo.c
@@ -227,11 +227,12 @@ void editorUpdateSyntax(erow *row) {
   int prev_sep = 1;
 
   int i = 0;
-  while (i < row->size) {
+  while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-    if (isdigit(c) && (prev_sep || prev_hl == HL_NORMAL)) {
+    if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+        (c == '.' && prev_hl == HL_NUMBER)) {
       row->hl[i] = HL_NUMBER;
       i++;
       prev_sep = 0;
